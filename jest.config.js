717879d143bce920ae1eaa4f606bc794b1a7d3a3
gkontogeorgos <georgioskont@hotmail.gr>
2024-06-16 14:51:39 +0300

    remove comments

--- jest.config.js
+++ jest.config.js
@@ -4,17 +4,5 @@ module.exports = {
   moduleNameMapper: {
     '\\.(css|sass)$': 'identity-obj-proxy',
   },
-  transformIgnorePatterns: [
-    '/node_modules/(?!@mui)/', // Adjust this regex pattern as needed
-  ], //   // Jest setup
-  //   moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-  //   moduleNameMapper: {
-  //     '^src/(.*)$': '<rootDir>/src/$1',
-  //     // Add other mappings as needed
-  //   },
-  //   transform: {
-  //     '^.+\\.(js|jsx)$': 'babel-jest',
-  //     // '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js', // Example path, adjust as per your project setup
-  //   },
-  //   // Add any other Jest configuration as needed
+  transformIgnorePatterns: ['/node_modules/(?!@mui)/'],
 };
