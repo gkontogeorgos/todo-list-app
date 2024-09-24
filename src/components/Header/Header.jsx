4518dfa90cd4ee8d904d0c604475d2b48d412536
gkontogeorgos <georgioskont@hotmail.gr>
2024-09-25 02:59:36 +0300

    test commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { Typography } from '@mui/material';
 import { HeaderBox } from './styles';
 
 const Header = () => {
-  console.log('test1');
+  console.log('test2');
   return (
     <HeaderBox>
       <Typography variant="h4" component="h1">
